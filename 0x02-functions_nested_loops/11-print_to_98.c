c621afd10ead3fbc2b4d99ac4831144a295f4f56
solomon244 <slegesse2444@gmail.com>
2022-03-15 19:14:38 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 /**
  * print_to_98 - print all natural numbers from input to 98
- * @n: starting point 
+ * @n: starting point
+ *
+ * Return: void returns nothing.
  */
 
 void print_to_98(int n)
